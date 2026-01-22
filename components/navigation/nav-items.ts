a4a08e035d4f08fff1ffd5fc7e5ac3871fd7f701
Abdelcrks <abdelmoumenberkat@gmail.com>
2026-01-22 16:14:33 +0100

    add btn for navbar mobile & desktop for contacts

--- components/navigation/nav-items.ts
+++ components/navigation/nav-items.ts
@@ -1,9 +1,10 @@
-import { Inbox, User, Users } from "lucide-react";
+import { Inbox, NotebookTabs, User, Users } from "lucide-react";
 
 export const NAV_ITEMS = [
   { href: "/matching", label: "Matching", icon: Users },
   { href: "/requests", label: "Requests", icon: Inbox },
   { href: "/profile", label: "Profile", icon: User },
+  { href: "/contacts", label : "Contacts", icon : NotebookTabs}
 ] as const
 
 
